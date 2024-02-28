2f194e82d3f09ebce57ed0c9498cb93f795d020c
denisbelenko <denisdel123@>
2024-02-28 18:49:12 +0700

    создал сущность продукта

--- trainingApp/models.py
+++ trainingApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Teacher(models.Model):
+    ...
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Наименование курса')
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name='Преподаватель')
+    start_products = models.DateTimeField(**NULLABLE, verbose_name='Начало курса')
+    cost = models.CharField(**NULLABLE, verbose_name='Стоимость курса')
+
