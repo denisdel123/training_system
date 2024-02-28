5e2af18694572b470cf9645d40d80ecdc18a00d3
denisbelenko <denisdel123@>
2024-02-28 18:59:33 +0700

    создал сущность преподавателя

--- trainingApp/models.py
+++ trainingApp/models.py
@@ -4,12 +4,21 @@ NULLABLE = {'null': True, 'blank': True}
 
 
 class Teacher(models.Model):
-    ...
+    first_name = models.CharField(max_length=30, verbose_name='Имя')
+    last_name = models.CharField(max_length=30, verbose_name='Фамилия')
+    photo = models.ImageField(upload_to='teacher_photo/', verbose_name='Фотография', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name}'
+
+    class Meta:
+        verbose_name = 'Преподаватель'
+        verbose_name_plural = 'Преподаватели'
 
 
 class Course(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование курса')
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name='Преподаватель')
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name='Автор курса')
     start_products = models.DateTimeField(**NULLABLE, verbose_name='Начало курса')
     cost = models.CharField(**NULLABLE, verbose_name='Стоимость курса')
 
